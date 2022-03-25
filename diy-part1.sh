1233f7a94ec6fc37232294b711e0250807752162
fantasysk <84770364+lisankai93@users.noreply.github.com>
2022-03-25 14:33:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,6 +17,3 @@
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/vssr/
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/vssr/
