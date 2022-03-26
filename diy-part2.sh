563aa9328b010295c63bddab39c75314b178d1a0
fantasysk <84770364+lisankai93@users.noreply.github.com>
2022-03-26 10:38:57 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,5 +20,4 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
 
-
-cd openwrt && ./scripts/feeds install -a
+./scripts/feeds install -a
