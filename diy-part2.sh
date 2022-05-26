e048cdace88d605604806d96a6f007a8cbda8308
fantasysk <84770364+lisankai93@users.noreply.github.com>
2022-05-26 15:09:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,12 +13,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
+git clone https://github.com/jerrykuku/lua-maxminddb.git package
 
-rm -rf package/lean/luci-theme-argon
-
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-
-sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
+git clone https://github.com/jerrykuku/luci-app-vssr.git package
 
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
